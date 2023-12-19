9dde48026a987a42f49aa6b4398426e3ae243438
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-12-19 23:34:05 +0500

    Update PROJECT.c

--- PROJECT.c
+++ PROJECT.c
@@ -448,3 +448,269 @@ void computer(char board[SIZE][SIZE], int mode)
             system("cls");
 
             tictac_name();
+            
+             if (mode == 2)
+                easy_move(board);
+            if (mode == 3)
+                best_move(board);
+
+            printBoard(board);
+
+            if (winner(board, 'X'))
+            {
+                printf(RED "Computer wins\n " RESET);
+                break;
+            }
+
+            if (isBoardFull(board))
+            {
+                printf(BLUE "Game is draw\n" RESET);
+                break;
+            }
+        }
+        else
+        {
+            printf(YELLOW "Invalid input.\n" RESET);
+        }
+    }
+    return;
+}
+
+void multiplayer(char board[SIZE][SIZE])
+{
+    tictac_name();
+    char player = 'X';
+    char move[10];
+    int row, col;
+    while (1)
+    {
+        system("cls");
+        tictac_name();
+        printBoard(board);
+        if (player == 'X')
+        {
+            printf("player" RED " %c " RESET "enter move ", player);
+        }
+        else
+        {
+            printf("player" BLUE " %c " RESET "enter move ", player);
+        }
+        scanf("%s", move);
+
+        if (strcmp(move, "s") == 0)
+        {
+            save(board);
+            continue;
+        }
+        else if (strcmp(move, "l") == 0)
+        {
+            load(board);
+            continue;
+        }
+        else if (strcmp(move, "e") == 0)
+        {
+            return;
+        }
+        else if (strcmp(move, "r") == 0)
+        {
+            reset(board);
+            continue;
+        }
+
+        row = (atoi(move) - 1) / 3;
+        col = (atoi(move) - 1) % 3;
+
+        if (isValid(board, row, col))
+        {
+            board[row][col] = player;
+        }
+        else
+        {
+            printf(YELLOW "Invalid input, Enter correct move\n" RESET);
+            Sleep(1000);
+            player = (player == 'X') ? 'O' : 'X';
+        }
+
+        if (winner(board, player))
+        {
+            printf(GREEN "Congratulations player %c won the game!\n" RESET, player);
+            return;
+        }
+        if (isBoardFull(board))
+        {
+            printf(BLUE "Game is Draw\033" RESET);
+            return;
+        }
+
+        player = (player == 'X') ? 'O' : 'X';
+        system("cls");
+    }
+}
+
+int RandomNumber()
+{
+    return rand() % 100 + 1;
+}
+
+// Function to check if the user's guess is correct
+int isCorrect(int numberToGuess, int userGuess)
+{
+    if (userGuess == numberToGuess)
+    {
+        return 1; // Guess is correct
+    }
+    else if (userGuess > numberToGuess)
+    {
+        printf("Too high! Try again.\n");
+    }
+    else
+    {
+        printf("Too low! Try again.\n");
+    }
+    return 0; // Guess is incorrect
+}
+
+void guessing_the_number()
+{
+    char exitChar;
+    int numberToGuess, userGuess, attempts = 0;
+
+    // Set seed for random number generation
+    srand(time(NULL));
+
+    do
+    {
+        // Generate a random number between 1 and 100
+        numberToGuess = RandomNumber();
+
+        printf(BLUE "^^^^^^^^^^^^^ Guess the Number ^^^^^^^^^^^^\n\n" RESET);
+        printf("Try to guess the number between 1 and 100.\n");
+        do
+        {
+            printf("Enter your guess (or enter 'e' to go back to the main menu): ");
+            if (scanf("%d", &userGuess) != 1)
+            {
+                // Invalid input, clear the input buffer
+                scanf("%s");
+                exitChar = getchar();
+                if (exitChar == 'e' || exitChar == 'E')
+                {
+                    return; // Exit the guessing game and return to the main menu
+                }
+                else
+                {
+                    printf("Invalid input. Please enter a number or 'e' to go back to the main menu.\n");
+                    continue;
+                }
+            }
+
+            attempts++;
+
+        } while (!isCorrect(numberToGuess, userGuess));
+
+        printf(GREEN "Congratulations! You guessed the number in %d attempts.\n" RESET, attempts);
+        Sleep(1000);
+        system("cls");
+    } while (1);
+}
+
+int generateComputerChoice()
+{
+    return rand() % 5 + 1; // 1: Rock, 2: Paper, 3: Scissors, 4: Lizard, 5: Spock
+}
+
+// Function to determine the winner based on choices on rock paper
+int determineWinner(int userChoice, int computerChoice)
+{
+    if ((userChoice == 1 && (computerChoice == 3 || computerChoice == 4)) ||
+        (userChoice == 2 && (computerChoice == 1 || computerChoice == 5)) ||
+        (userChoice == 3 && (computerChoice == 2 || computerChoice == 4)) ||
+        (userChoice == 4 && (computerChoice == 2 || computerChoice == 5)) ||
+        (userChoice == 5 && (computerChoice == 1 || computerChoice == 3)))
+    {
+        return 1; // User wins
+    }
+    else if (userChoice == computerChoice)
+    {
+        return 0; // It's a tie
+    }
+    else
+    {
+        return -1; // Computer wins
+    }
+}
+
+// Function to print choices
+void printChoices()
+{
+    printf("Choices:\n"
+           "\t1. Rock\n"
+           "\t2. Paper\n"
+           "\t3. Scissors\n"
+           "\t4. Lizard\n"
+           "\t5. Spock\n");
+}
+
+void rock_paper()
+{
+    int userChoice, computerChoice, result;
+    char playAgain;
+
+    // Set seed for random number generation
+    srand(time(NULL));
+
+    printf("^^^^^^^^^^ Welcome to Rock, Paper, Scissors, Lizard, Spock! ^^^^^^\n");
+
+    do
+    {
+        // Get user choice
+        printChoices();
+        printf("Enter your choice (1-5): ");
+        scanf("%d", &userChoice);
+
+        if (userChoice < 1 || userChoice > 5)
+        {
+            printf(YELLOW "Invalid choice. Please enter a number between 1 and 5.\n" RESET);
+            continue;
+        }
+
+        // Generate computer choice
+        computerChoice = generateComputerChoice();
+
+        // Display choices
+        printf("You chose: %d\n", userChoice);
+        printf("Computer chose: %d\n", computerChoice);
+
+        // Determine the winner
+        result = determineWinner(userChoice, computerChoice);
+
+        if (result == 1)
+        {
+            printf(GREEN "Congratulations! You win!\n" RESET);
+        }
+        else if (result == -1)
+        {
+            printf(RED "Sorry, you lose. Better luck next time!\n" RESET);
+        }
+        else
+        {
+            printf(BLUE "It's a tie. Try again!\033[0m\n" RESET);
+        }
+
+        // Ask if the user wants to play again
+        printf("\nDo you want to play again? (y/n): ");
+        scanf(" %c", &playAgain);
+        system("cls");
+    } while (playAgain == 'y' || playAgain == 'Y');
+    return;
+}
+
+void file_func()
+{
+    printf(
+        GREEN "\t\t\t s for save\n" RESET
+            BLUE "\t\t\t r for reset\n" RESET
+                YELLOW "\t\t\t l for load\n" RESET
+                    RED "\t\t\t e for exit\n\n" RESET);
+}
+
